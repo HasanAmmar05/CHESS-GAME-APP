3d6f48d2c7af2a723df5b85d816454f3745aacca
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-06 18:12:38 +0800

    Added multiple functions for drag and drop

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ function createBoard() {
         square.classList.add('square');
         // square.classList.add('beige');
         square.innerHTML = startPiece;
+        square.firstChild && square.firstChild.setAttribute('draggable', true)
         square.setAttribute('square-id', i);
 
         const row = Math.floor( (63 - i) / 8) + 1;
@@ -37,9 +38,42 @@ function createBoard() {
             square.firstChild.firstChild.classList.add("black")
         }
 
+        if (i >= 48) {
+            square.firstChild.firstChild.classList.add("white")
+        }
+
         GameBoard.append(square);
 
     })
+}
+
+const allSquares = document.querySelectorAll("#gameboard .square");
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart);
+    square.addEventListener('dragover', dragOver);
+    square.addEventListener('drop', dragDrop);
+})
+
+let startPositionId
+let draggedElement
+
+function dragStart (e) {
+    startPositionId = e.target.parentNode.getAttribute('square-id');
+    draggedElement = e.target
+}
+
+function dragOver (e) {
+    e.preventDefault();
+}
+
+
+function dragDrop(e){
+    e.stopPropagation()
+    const Taken = e.target.classList.contains('piece')
+
+    // e.target.parentNode.append(draggedElement);
+    // e.target.removr()
 }
 
 createBoard();
