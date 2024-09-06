7748795f8137e145f1bc06f238b2be8afe39dd0c
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-06 12:27:01 +0800

    Assigned all chess pieces to their svg's in the pieces file

---
+++ main.js
@@ -0,0 +1,9 @@
+const GameBoard = document.querySelector("#gameboard");
+const PlayerDisplay = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+const width = 8;
+
+const startPieces = [
+    
+
+]
